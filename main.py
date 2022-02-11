c1783062cf54bfe59241fa6df1d0ff5e57d5b8a4
angrypy <wopker11@gmail.com>
2022-02-11 22:04:29 +0300

    Second

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ from config import tg_bot_token
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher, filters
 from aiogram.utils import executor
+
 from aiogram.utils.markdown import hbold, hlink
